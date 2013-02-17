af2f66a2323ee6e60270e17feec9ae3ca721be5d
Tobias Mueller <Tobias_Mueller@twam.info>
2013-02-17 18:45:38 +0100

    Sublime for Linux changes

--- sublime/sublime.zsh
+++ sublime/sublime.zsh
@@ -1,6 +1,12 @@
-# OS X
-if [[ -x "/Applications/Sublime Text.app" ]]
+# Search for Sublime on OS X
+if [[ -x /Applications/Sublime\ Text.app ]]
 then
-    alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+  alias subl=/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl
+fi
+
+# Search for Sublime on Linux
+if [[ -x ~/sublime_text_3/sublime_text ]]
+then
+  alias subl=~/sublime_text_3/sublime_text
 fi
 
