2c1da98373c8864b489e4fa5810c41dc63d94464
Tobias Mueller <Tobias_Mueller@twam.info>
2013-02-18 14:50:46 +0100

    pos1/end keybindungs on some strange linux machines

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -9,10 +9,15 @@ bindkey "^[[3~" delete-char
 bindkey "^[3;5~" delete-char
 bindkey "\e[3~" delete-char
 
-
 # Use Ctrl+V followed by the key to find these out on a shell
-bindkey "^[[1~" beginning-of-line
-bindkey "^[[4~" end-of-line
+bindkey "^[[1~" beginning-of-line	# home on OS X (with custom terminal settings)
+bindkey "^[[1;5D" beginning-of-line 	# ctrl+left on linux
+bindkey "^[OH" beginning-of-line 	# home on linux
+
+bindkey "^[[4~" end-of-line		# end on OS X (with custom terminal settings)
+bindkey "^[OF" end-of-line		# end on linux
+bindkey "^[[1;5C" end-of-line		# ctrl+right on linux
+
 bindkey "^[[A" up-line-or-history
 bindkey "^[[B" down-line-or-history
 bindkey "^[[D" backward-char
