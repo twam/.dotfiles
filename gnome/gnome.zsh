5cd5a900e58cd4566a7ce7bd48ce3865e111b437
Tobias Müller <Tobias_Mueller@twam.info>
2014-03-19 07:19:09 +0100

    add 'open' alias for GNOME to gnome-open

--- gnome/gnome.zsh
+++ gnome/gnome.zsh
@@ -1,5 +1,11 @@
 zmodload zsh/regex
 
+if [[ -x /usr/bin/gnome-open ]]
+then
+	alias open="gnome-open"
+fi
+
+
 # Enable color support for gnome-terminal by setting TERM correctly
 if [[ ($COLORTERM = "gnome-terminal") && (! "$TERM" -regex-match "-256color$") ]] then
   export TERM="$TERM-256color"
