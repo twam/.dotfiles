61b5200d7be148ea95c4635a941d6ed7b5ecaade
Tobias Müller <Tobias_Mueller@twam.info>
2014-07-10 14:32:26 +0200

    improved terminal title handling

--- termsupport/termsupport.zsh
+++ termsupport/termsupport.zsh
@@ -7,6 +7,7 @@
 function title {
   if [[ "$TERM" == screen* ]]; then
     print -Pn "\ek$1:q\e\\" #set screen hardstatus, usually truncated at 20 chars
+    print -Pn "\eP\e]0;[$1]\a\e\\" # set the xterm window title by escaping (http://serverfault.com/questions/35085/set-xterm-putty-window-title-when-using-screen)
   elif [[ "$TERM" == xterm* ]] || [[ $TERM == rxvt* ]] || [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
     print -Pn "\e]2;$2:q\a" #set window name
     print -Pn "\e]1;$1:q\a" #set icon (=tab) name (will override window name on broken terminal)
@@ -14,18 +15,24 @@ function title {
 }
 
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
-ZSH_THEME_TERM_TITLE_IDLE="%n@%m: %~"
+ZSH_THEME_TERM_TAB_TITLE_SSH_IDLE="%15<..<%~%<<" #15 char left truncated PWD
+ZSH_THEME_TERM_TITLE_IDLE="%~"
+ZSH_THEME_TERM_TITLE_SSH_IDLE="%n@%m: %~"
 
 # Appears when you have the prompt
 function omz_termsupport_precmd {
-  title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
+  if [ $SSH_CONNECTION ]; then
+    title $ZSH_THEME_TERM_TAB_TITLE_SSH_IDLE $ZSH_THEME_TERM_TITLE_SSH_IDLE
+  else
+    title $ZSH_THEME_TERM_TAB_TITLE_IDLE $ZSH_THEME_TERM_TITLE_IDLE
+  fi
 }
 
 # Appears at the beginning of (and during) of command execution
 function omz_termsupport_preexec {
   emulate -L zsh
   setopt extended_glob
-  local CMD=${1[(wr)^(*=*|sudo|ssh|rake|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
+  local CMD=${1[(wr)^(*=*|screen|sudo|ssh|rake|-*)]} #cmd name only, or if this is sudo or ssh, the next cmd
   title "$CMD" "%100>...>${2:gs/%/%%}%<<"
 }
 
