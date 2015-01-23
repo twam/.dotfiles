465a0f0bc7019dd9866f735267b8b55be4266a01
Tobias Müller <Tobias_Mueller@twam.info>
2015-01-23 07:17:08 +0100

    improved sublime finding

--- sublime/sublime.zsh
+++ sublime/sublime.zsh
@@ -1,10 +1,16 @@
 # Search for Sublime on OS X
+if [[ "$OSTYPE" =~ "^darwin" ]]; then
+
 if [[ -x /Applications/Sublime\ Text.app ]]
 then
   SUBLIME_EXE=/Applications/Sublime\\\ Text.app/Contents/SharedSupport/bin/subl
 fi
 
+fi
+
 # Search for Sublime on Linux
+if [[ "$OSTYPE" =~ "^linux" ]]; then
+
 if [[ -x ~/sublime_text_3/sublime_text ]]
 then
   SUBLIME_EXE=~/sublime_text_3/sublime_text
@@ -16,4 +22,6 @@ then
 	export SVN_EDITOR="$SUBLIME_EXE --wait"
 	export GIT_EDITOR="$SUBLIME_EXE --wait"
 	alias subl=$SUBLIME_EXE
+fi
+
 fi
