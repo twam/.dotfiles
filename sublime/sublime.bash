95109e3ecd5fa5eb6f589614e6df7531137c4b80
Tobias Müller <Tobias_Mueller@twam.info>
2019-08-04 16:40:47 +0200

    Improve subl detection

--- sublime/sublime.bash
+++ sublime/sublime.bash
@@ -1,1 +1,38 @@
-sublime.zsh
+# Search for Sublime on OS X
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+
+if [[ -x /Applications/Sublime\ Text.app ]]
+then
+  SUBLIME_EXE=/Applications/Sublime\\\ Text.app/Contents/SharedSupport/bin/subl
+fi
+
+fi
+
+# Search for Sublime on Windows
+if [[ "$OSTYPE" =~ ^cygwin ]]; then
+
+if [[ -x C:/Programme/Sublime\ Text\ 3/subl.exe ]]
+then
+  SUBLIME_EXE=C:/Programme/Sublime\\\ Text\\\ 3/subl.exe
+fi
+
+fi
+
+# Search for Sublime on Linux
+if [[ "$OSTYPE" =~ ^linux ]]; then
+
+if [[ -x ~/sublime_text_3/sublime_text ]]
+then
+  SUBLIME_EXE=~/sublime_text_3/sublime_text
+fi
+
+fi
+
+if [[ ! -z "$SUBLIME_EXE" ]]
+then
+	export EDITOR="$SUBLIME_EXE --wait"
+	export SVN_EDITOR="$SUBLIME_EXE --wait"
+	export GIT_EDITOR="$SUBLIME_EXE --wait"
+	alias subl=$SUBLIME_EXE
+fi
+
--- sublime/sublime.zsh
+++ sublime/sublime.zsh
@@ -1,38 +1,11 @@
-# Search for Sublime on OS X
-if [[ "$OSTYPE" =~ ^darwin ]]; then
-
-if [[ -x /Applications/Sublime\ Text.app ]]
-then
-  SUBLIME_EXE=/Applications/Sublime\\\ Text.app/Contents/SharedSupport/bin/subl
+if (( ${+aliases[subl]} )) ; then
+  export EDITOR="`whence subl` --wait"
+  export SVN_EDITOR="`whence subl` --wait"
+  export GIT_EDITOR="`whence subl` --wait"
 fi
 
+if (( ${+commands[subl]} )) ; then
+  export EDITOR="subl --wait"
+  export SVN_EDITOR="subl --wait"
+  export GIT_EDITOR="subl --wait"
 fi
-
-# Search for Sublime on Windows
-if [[ "$OSTYPE" =~ ^cygwin ]]; then
-
-if [[ -x C:/Programme/Sublime\ Text\ 3/subl.exe ]]
-then
-  SUBLIME_EXE=C:/Programme/Sublime\\\ Text\\\ 3/subl.exe
-fi
-
-fi
-
-# Search for Sublime on Linux
-if [[ "$OSTYPE" =~ ^linux ]]; then
-
-if [[ -x ~/sublime_text_3/sublime_text ]]
-then
-  SUBLIME_EXE=~/sublime_text_3/sublime_text
-fi
-
-fi
-
-if [[ ! -z "$SUBLIME_EXE" ]]
-then
-	export EDITOR="$SUBLIME_EXE --wait"
-	export SVN_EDITOR="$SUBLIME_EXE --wait"
-	export GIT_EDITOR="$SUBLIME_EXE --wait"
-	alias subl=$SUBLIME_EXE
-fi
-
