988a0bd0f3e8e58588a2504209a457bd20eae2f5
Tobias Müller <Tobias_Mueller@twam.info>
2022-11-25 09:05:34 +0100

    Add OSX configure script

---
+++ osx/configure.sh
@@ -0,0 +1,4 @@
+defaults write com.apple.dock autohide-time-modifier -float 0.15
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock tilesize -int 48
+killall Dock 
